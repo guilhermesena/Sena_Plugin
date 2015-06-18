bfd6d7ee7538712b7cfe347f790d944658ddff80
Guilherme Sena <guilhermes@palantir.com>
2015-06-18 13:53:00 -0300

    Adding Kalmann filter and Stack Parsing

--- Sena_Plugin.java
+++ Sena_Plugin.java
@@ -15,48 +15,97 @@ public class Sena_Plugin implements PlugInFilter {
 	private int IMAGE_HEIGHT;
 	private int [][] visited;
 	
+	private boolean parseImage(ImageProcessor ip) {
+		
+		IJ.showMessage("Verifying if stack is correctly parsed");
+		
+		ImagePlus imp = IJ.getImage();
+		if (imp.getStackSize()==1) {
+			IJ.error("Stack required"); 
+			return false;
+		}
+		
+		
+		int size = imp.getStack().getSize();
+		for(int i = 0; i < size; i++) {
+			IJ.setSlice(i);
+			if(IJ.getImage().getType() != ImagePlus.GRAY8) {
+				IJ.error("Image number "+i+" is not 8-bit grayscale");
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
 	private void initFilters(ImageProcessor ip) {
 		IJ.showMessage("Adding Filters");
+		
+		IJ.showMessage("Adding Kalman filter");
+		Kalman_Stack_Filter ksf = new Kalman_Stack_Filter();
+		ksf.run("");
+		
+		IJ.showMessage("Auto Thresholding...");
 		ip.autoThreshold();
+		
+		IJ.showMessage("Eroding...");
 		ip.erode();
+		
 	}
 	
 	private void segmentImage(ImageProcessor ip) {
 		IJ.showMessage("Starting image segmentation");
 		int numParticles = 0;
+		int MIN_SIZE = 50;
+		
+		int SMALLEST = Integer.MAX_VALUE;
+		int LARGEST = -1;
+		
 		for(int i = 0; i < IMAGE_WIDTH; i++) {
 			for(int j = 0; j < IMAGE_HEIGHT; j++) {
 				if(ip.getPixel(i, j) == 255 && visited[i][j] == 0) {
-					numParticles++;
-					dfs(i, j, ip);
+					int sz = dfs(i, j, ip);
+					if(sz > MIN_SIZE)
+						numParticles++;
+					
+					if(sz < SMALLEST) SMALLEST = sz;
+					if(sz > LARGEST) LARGEST = sz;
 				}
 			}
 		}
 		
-		IJ.showMessage("Found " +numParticles+" particles");
+		IJ.showMessage("Found " +numParticles+" particles. Smallest: "+SMALLEST+". Largest: "+LARGEST);
 	}
-	
+		
 	public void run(ImageProcessor ip) {
 		IJ.showMessage("Starting Plugin");
-		initFilters(ip);
 		
+		if(!parseImage(ip)) {
+			return;
+		}
+		
+		initFilters(ip);
 		segmentImage(ip);
 	}
 	
-	private void dfs(int x, int y, ImageProcessor ip){
+	private int dfs(int x, int y, ImageProcessor ip){
 		if(x < 0 || y < 0 || x >= IMAGE_WIDTH || y >= IMAGE_HEIGHT || ip.getPixel(x, y) != 255 || visited[x][y] == 1) {
-			return;
+			return 0;
 		}
 		
 		visited[x][y] = 1;
 		ip.putPixel(x, y, 128);
+		
+		int ans = 0;
+		
 		for(int u = -1; u <= 1; u++) {
 			for(int v = -1; v <= 1; v++) {
 				if(u == 0 && v == 0)
 					continue;
-				dfs(x+u,y+v,ip);
+				ans += dfs(x+u,y+v,ip);
 			}
 		}
+		return ans+1;
 	}
 
 	public int setup(String args, ImagePlus ip) {
